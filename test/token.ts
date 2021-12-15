1c57ea750454ee4630c680510ff7be5bea317ec2
Christopher Krogh <chris.krogh@outlook.com>
2021-12-15 15:38:32 -0400

    refactored burn test

--- test/token.ts
+++ test/token.ts
@@ -20,31 +20,28 @@ describe('Token', function () {
 
   it('Should mint tokens to user1', async function () {
     await token.mint(user1.address, '100');
+
     expect(await token.balanceOf(user1.address)).to.equal('100');
     expect(await token.totalSupply()).to.equal('100');
   });
 
   it('Should transfer tokens from user1 to user2', async function () {
     await token.mint(user1.address, '100');
-    expect(await token.balanceOf(user1.address)).to.equal('100');
+    const tx = await token.connect(user1).transfer(user2.address, '50');
 
-    await token.connect(user1).transfer(user2.address, '50');
+    expect(tx)
+      .to.emit(token, 'Transfer')
+      .withArgs(user1.address, user2.address, '50');
     expect(await token.balanceOf(user1.address)).to.equal('50');
     expect(await token.balanceOf(user2.address)).to.equal('50');
   });
 
-  it('Should burn tokens when received', async function () {
-    await token.mint(user1.address, '100');
-    const tx = await token.connect(user1).transfer(owner.address, '50');
+  it('Should be able to burn tokens ', async function () {
+    await token.mint(user1.address, '50');
+    await token.connect(user1).approve(owner.address, '50');
+    await token.burnFrom(user1.address, '50');
 
-    expect(tx)
-      .to.emit(token, 'Transfer')
-      .withArgs(user1.address, owner.address, '50');
-    expect(await token.balanceOf(user1.address)).to.equal('50');
-    expect(await token.balanceOf(owner.address)).to.equal('50');
-
-    await token.connect(owner).burn('50');
-    expect(await token.balanceOf(owner.address)).to.equal('0');
-    expect(await token.totalSupply()).to.equal('50');
+    expect(await token.balanceOf(user1.address)).to.equal('0');
+    expect(await token.totalSupply()).to.equal('0');
   });
 });
