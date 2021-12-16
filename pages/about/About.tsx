292aa6af1c0827682dc2373af47b8c02eff0f704
Christopher Krogh <chris.krogh@outlook.com>
2021-12-16 11:24:43 -0400

    initial about page

---
+++ pages/about/About.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Caribto - About</title>
+        <meta
+          name="description"
+          content="Learn more about the inspiration behind Caribto"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default About;
