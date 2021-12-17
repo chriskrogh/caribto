d375a83d24d552cf1bee9e3362e14f70c7725d87
Christopher Krogh <chris.krogh@outlook.com>
2021-12-16 22:01:36 -0400

    secondary button color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,12 +5,14 @@ import Typography from '../Typography';
 
 type ContainerProps = {
   fullWidth?: boolean;
+  secondary?: boolean;
 };
 const Container = styled.button<ContainerProps>`
   border: none;
   margin: 0;
   padding: 8px 16px;
-  background-color: #0984e3;
+  background-color: ${({ secondary }) =>
+    secondary ? 'rgba(255, 255, 255, 0.12)' : '#0984e3'};
   border-radius: 8px;
   cursor: pointer;
   width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
@@ -24,7 +26,7 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement> &
 const Button: React.FC<Props> = ({ children, ...props }) => {
   return (
     <Container {...props}>
-      <Typography as="h5" bold>
+      <Typography as="h5" bold secondary={props.disabled}>
         {children}
       </Typography>
     </Container>
