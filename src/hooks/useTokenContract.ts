807bf12ff048a06f97d0ee361c6b2f9a28673b1a
Christopher Krogh <chris.krogh@outlook.com>
2021-12-17 14:47:14 -0400

    using env var

--- src/hooks/useTokenContract.ts
+++ src/hooks/useTokenContract.ts
@@ -8,7 +8,7 @@ const buildTokenContract = (): TokenContract => {
   if (typeof window !== 'undefined') {
     const provider = new ethers.providers.JsonRpcProvider();
     return new ethers.Contract(
-      '0x5fbdb2315678afecb367f032d93f642f64180aa3', // process.env.TOKEN_CONTRACT_ADDRESS,
+      process.env.NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS,
       TokenArtifact.abi,
       provider.getSigner(),
     ) as TokenContract;
