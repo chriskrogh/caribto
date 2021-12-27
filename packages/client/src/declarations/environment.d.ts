7e8d3bc92817454d3905c836c05ba5d37b606207
Christopher Krogh <chris.krogh@outlook.com>
2021-12-27 09:30:02 -0400

    removed unused types

--- packages/client/src/declarations/environment.d.ts
+++ packages/client/src/declarations/environment.d.ts
@@ -2,8 +2,6 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS: string;
-      NEXT_PUBLIC_INFURA_ID: string;
-      NEXT_PUBLIC_RPC_URL: string;
     }
   }
 }
