cdc4776b0a0304e1ec3c9594f9ebc4a42f9d9fed
Christopher Krogh <chris.krogh@outlook.com>
2021-12-28 11:35:00 -0400

    removed server

--- packages/server/src/routers/Token/types.ts
+++
@@ -1,3 +0,0 @@
-export enum LOCAL_ROUTES {
-  MINT = '/mint',
-}
--- packages/server/src/routers/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-import { TokenRouter } from './Token';
-
-export const APIRouter = express.Router();
-
-APIRouter.use('/token', TokenRouter);
