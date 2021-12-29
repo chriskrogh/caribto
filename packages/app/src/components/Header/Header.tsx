7a8193f543e2d5658322e52983e9e69394effe12
Christopher Krogh <chris.krogh@outlook.com>
2021-12-29 08:29:33 -0400

    header with about link

--- packages/app/src/components/Header/Header.tsx
+++ packages/app/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -13,19 +14,28 @@ const Container = styled(Row)`
   flex: 0 1 ${IMAGE_SIZE}px;
 `;
 
+const ClickableRow = styled(Row)`
+  cursor: pointer;
+`;
+
 const Header: React.FC = () => {
   return (
     <Container justifyContent="space-between" alignItems="center">
-      <Row>
-        <Image
-          src="/assets/palm-tree-small.png"
-          width={IMAGE_SIZE}
-          height={IMAGE_SIZE}
-          alt="Palm tree"
-        />
-        <Spacer width={16} />
-        <Typography as="h4">Caribto</Typography>
-      </Row>
+      <Link href="/">
+        <ClickableRow>
+          <Image
+            src="/assets/palm-tree-small.png"
+            width={IMAGE_SIZE}
+            height={IMAGE_SIZE}
+            alt="Palm tree"
+          />
+          <Spacer width={16} />
+          <Typography as="h4">Caribto</Typography>
+        </ClickableRow>
+      </Link>
+      <Link href="/about">
+        <Typography as="a">About</Typography>
+      </Link>
     </Container>
   );
 };
